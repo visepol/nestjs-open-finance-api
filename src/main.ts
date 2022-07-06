ef955dc0c05164070b204caa7b1d89af65cbd112
Vinicius Lopes <vinicius@bitloom.net>
2022-07-06 00:54:27 -0300

    criada rota de health no controler de monitoria

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const upTime = new Date();
+  env.UP_TIME = upTime.toString();
 }
+
 bootstrap();
--- src/monitors/monitors.controller.ts
+++ src/monitors/monitors.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('monitors')
-export class MonitorsController {}
+export class MonitorsController {
+    @Get('health')
+    health(): Object {
+        return {
+            app_name: process.env.APP_NAME,
+            pid: process.pid,
+            up_time: process.env.UP_TIME
+        }
+      }
+}
