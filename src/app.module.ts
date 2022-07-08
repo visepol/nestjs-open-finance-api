6378dc2696b87476ab50297db4e32fc4cefe1ebc
Vinicius Lopes <vinicius@bitloom.net>
2022-07-07 21:15:29 -0300

    Adicionado Doc Builder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MonitorsController } from './monitors/monitors.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, MonitorsController],
-  providers: [AppService],
+  controllers: [MonitorsController],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, 
+    new DocumentBuilder()
+    .setTitle('Open Finance API')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build()
+  ));
+
   await app.listen(3000);
 
   const upTime = new Date();
